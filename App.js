842a9ef0b81cf51ba505817f522239eab0c99868
jitu <jjbatiya786@gmail.com>
2025-02-22 20:39:39 +0530

    app.js

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from "react";
 import { Provider } from 'react-redux';
 import { NavigationContainer } from '@react-navigation/native';
 import store from './src/redux/store';
